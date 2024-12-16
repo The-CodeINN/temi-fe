7ec40007dfae881d450d94a7c5af1ccb27aa2311
Richard Emijere <emijere.richard@gmail.com>
2024-12-16 09:59:51 +0100

    new additions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <main className='flex min-h-screen items-center justify-center text-5xl text-[#009ce0] font-bold'>
